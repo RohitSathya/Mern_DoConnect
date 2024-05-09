00b9fc97556043b8e33957097c1d5718d8dc4813
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-05-09 12:09:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,27 +5,12 @@ const app=express()
 const UserRoute=require('./router/UserRoute')
 const AdminRoute=require('./router/AdminRoute')
 const chatRoute=require('./router/ChatRoute')
-const http=require('http')
-const socketio=require('socket.io')
-const server=http.createServer(app)
-const io=socketio(server,{
-     cors:{
-          origin:["http://localhost:3001"],
-    
-          credentials:true
-     }
-})
 app.use(express.json())
 app.use(cors())
 app.use('/api/user',UserRoute)
 app.use('/api/admin',AdminRoute)
 app.use('/api/chat',chatRoute)
-io.on("connection",(socket)=>{
-     console.log('conn')
-     socket.on('msges',(data)=>{
-         console.log(data)
-     })
- })
+
 
 mongoose.connect('mongodb+srv://admin:sunsetwest1234RRR@royoapi.3qmdrjq.mongodb.net/Doconnect?retryWrites').then(()=>{
 

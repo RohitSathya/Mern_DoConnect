12bbb48eb5d99c81f916eaf6883e678114840eac
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-05-13 11:23:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const app=express()
 const UserRoute=require('./router/UserRoute')
 const AdminRoute=require('./router/AdminRoute')
 const chatRoute=require('./router/ChatRoute')
+
 app.use(express.json())
 app.use(cors())
 app.use('/api/user',UserRoute)
@@ -12,8 +13,8 @@ app.use('/api/admin',AdminRoute)
 app.use('/api/chat',chatRoute)
 
 
-mongoose.connect('mongodb+srv://admin:sunsetwest1234RRR@royoapi.3qmdrjq.mongodb.net/Doconnect?retryWrites').then(()=>{
+mongoose.connect('mongodb+srv://admin:sunsetwest1234RRR@royoapi.3qmdrjq.mongodb.net/doconn?retryWrites=true&w=majority&appName=RoyoApi').then(()=>{
 
      console.log('db connected')
-     app.listen(3000,()=>console.log('server running'))
+     app.listen(5000,()=>console.log('server running'))
 })
